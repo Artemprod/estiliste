fc98b4c8deabacb66b15fd47a08a3d21c492cc57
artemprod <artemprod1@gmail.com>
2024-05-15 16:52:42 +0400

    first commit

---
+++ bot/handlers/command_handler.py
@@ -0,0 +1,13 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+# from bot.keyboards.inline_keyboards import crete_inline_keyboard_options
+
+router = Router()
+
+
+@router.message(CommandStart(), )
+async def process_start_command(message: Message):
+    # option_keyboard = crete_inline_keyboard_options()
+    await message.answer(text='Закинь фотку')
